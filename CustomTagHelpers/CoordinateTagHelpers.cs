ed1fd1762d8307dddbc19ca9a47c8aedfcee230f
my name is doraemon <tuandinhbi75@gmail.com>
2023-09-02 01:06:48 +0700

    some changes

--- CustomTagHelpers/CoordinateTagHelpers.cs
+++ CustomTagHelpers/CoordinateTagHelpers.cs
@@ -3,12 +3,24 @@
 namespace WebEmpty.CustomTagHelpers
 {
     [HtmlTargetElement("div", Attributes = "theme")]
-    public class CoordinateTagHelpers
+    public class CoordinateTagHelpers : TagHelper
     {
         public string Theme { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             context.Items["theme"] = Theme;
+        }
+
+    }
+
+    [HtmlTargetElement("button", ParentTag = "div")]
+    [HtmlTargetElement("a", ParentTag = "div")]
+    public class ChildThemeTH : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.Items.ContainsKey("theme"))
+                output.Attributes.SetAttribute("class", $"btn btn-{context.Items["theme"]}");
         }
     }
 }
